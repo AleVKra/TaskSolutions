4fd78294e58e204d78a65acaf39bda174bc32552
AleVKra <aleVKra01@gmail.com>
2024-10-04 18:21:10 +0300

    git commit: циклы while и for

---
+++ JavaTaskSolution/src/main/java/solutionTasks/telephonTask/TelephoneBook.java
@@ -0,0 +1,57 @@
+package solutionTasks.telephonTask;
+
+
+//2. Написать простой класс Телефонный Справочник, который хранит в себе список фамилий и
+//  телефонных номеров.
+//  В этот телефонный справочник с помощью метода add() можно добавлять записи,
+//  а с помощью метода getPhoneNumberBySurname() искать номер телефона по фамилии.
+//  Следует учесть, что под одной фамилией может быть несколько  телефонов
+//  (в случае однофамильцев), тогда при запросе такой фамилии должны выводиться все
+//  телефоны. Желательно не добавлять  лишний функционал (дополнительные поля (имя,
+//  отчество, адрес), взаимодействие с пользователем через консоль и т.д).
+//  Консоль использовать только для вывода результатов проверки телефонного справочника.
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.*;
+
+// телефонный справочник
+@Getter
+@NoArgsConstructor
+//@AllArgsConstructor
+public class TelephoneBook {
+
+//    private String surname;
+//    private Set<String> phoneNumber;
+
+//    Map<String, Set<String>> map = new HashMap<>();
+//    private Object String;
+
+//    public TelephoneBook(Map<String, Set<String>> map) {
+//        this.map = map;
+//    }
+
+    // добавить пользователя
+//    public void add(Map<String, Set<String>> map) {
+//        map.put(surname, phoneNumber);
+//    }
+
+//    получить номер пользователя по фамилии
+
+//    public String getPhoneNumberBySurname(String surname) {
+//        Set<String> getNumbers = null;
+
+//        if ()
+//        Set set = map.entrySet();
+//        Iterator iterator = set.iterator();
+//
+//        while (iterator.hasNext()) {
+//            Map.Entry item = (Map.Entry) iterator.next();
+//            System.out.println("Фамилия: " + item.getKey() + " : " + "'" + "'" + item.getValue() + "'}");
+//        }
+//        return ;
+//    }
+
+}
